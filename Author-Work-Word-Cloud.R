ef0d0cd273e1c7c18cf842f884f4cd040925572b
Tze Chang (Justin) Ng <ngtzechang@gmail.com>
2022-03-06 15:33:20 +0000

    Shiny App Tidied
    Shiny App instructions added and results were separated into two layers

--- Author-Work-Word-Cloud.R
+++ Author-Work-Word-Cloud.R
@@ -10,38 +10,43 @@ ui <- fluidPage(
   
   sidebarLayout(
     sidebarPanel(
-      fileInput(inputId = "file", label = "Upload CSV"), accept = ".csv"),
-#NEED TO ADD INSTRUCTIONS
-      mainPanel(tableOutput(outputId = "table"),
-        plotOutput(outputId = "plot"))
+      h5("Please upload CSV file containing data frame of publication titles. Then, provide the column name which contain the data of interest"),
+      fileInput(inputId = "file", label = "Upload CSV", accept = ".csv"),
+      textInput(inputId = "name", label = "Type column name containing publication titles", "Title")),
+
+    fluidPage(tabsetPanel(
+      tabPanel("Word Count Plot", plotOutput(outputId = "plot")),
+      tabPanel("Data Table", mainPanel(tableOutput(outputId = "table"))),
+      )
     )
   )
+)
 
 #Server
 server <- function(input, output) {
-#FIND A WAY TO DISPLAY PLOT BEFORE THE TABLE OR SEPERATE INTO TWO PAGES  
+  #FIND A WAY TO DISPLAY PLOT BEFORE THE TABLE OR SEPERATE INTO TWO PAGES  
   output$plot <- renderPlot({
     inputFile <- input$file
     csv <- read.csv(inputFile$datapath)
     
     clean_dat <- csv %>% 
-      select(Title) %>% 
-      unnest_tokens(output = word, input = Title) %>%
+      select(input$name) %>% 
+      unnest_tokens(output = word, input = input$name) %>%
       anti_join(stop_words) %>%
       count(word, sort = TRUE)
     
     top10_clean_dat <- clean_dat[1:10,] 
     
     plot <- top10_clean_dat %>%
-        mutate(word = reorder(word,n)) %>%
-        ggplot(aes(word, n)) +
-        geom_col() +
-        coord_flip() +
-        labs(x = "Words \n", y = "\n Counts", title = "Frequent Words on Author's Publications Title \n") +
-        geom_text(aes(label = n), hjust = 1.5, color = "white", fontface = "bold") +
-        theme_grey()
+      mutate(word = reorder(word,n)) %>%
+      ggplot(aes(word, n)) +
+      geom_col() +
+      coord_flip() +
+      labs(x = "Words \n", y = "\n Counts", title = "Frequent Words on Author's Publications Title \n") +
+      geom_text(aes(label = n), hjust = 1.5, color = "white", fontface = "bold") +
+      theme_grey()
     plot
-    })
+  })
   
   output$table <- renderTable({
     if(is.null(input$file)){paste("No CSV File Uploaded")}
